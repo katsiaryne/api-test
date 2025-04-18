5f872596f4f808c70e7429e1fa8f119f87abf2e0
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2025-04-18 10:27:10 +0300

    fix: add spec consts

--- reqres-example/src/test/java/in/res/config/Specification.java
+++ reqres-example/src/test/java/in/res/config/Specification.java
@@ -6,13 +6,15 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import static in.res.util.SpecificationConstants.BASE_URI;
+import static in.res.util.SpecificationConstants.RESPONSE_TIME;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public final class Specification {
     public static RequestSpecification requestSpecification = new RequestSpecBuilder()
             .setContentType(ContentType.JSON)
-            .setBaseUri("https://reqres.in/api")
+            .setBaseUri(BASE_URI)
             .build();
 
     public static ResponseSpecification responseSpecificationWithContent(int expectedStatusCode, String schema){
@@ -20,14 +22,14 @@ public final class Specification {
                 .expectStatusCode(expectedStatusCode)
                 .expectContentType(ContentType.JSON)
                 .expectBody(matchesJsonSchemaInClasspath(schema))
-                .expectResponseTime(lessThanOrEqualTo(3000L))
+                .expectResponseTime(lessThanOrEqualTo(RESPONSE_TIME))
                 .build();
     }
 
     public static ResponseSpecification responseSpecification(int expectedStatusCode){
         return new ResponseSpecBuilder()
                 .expectStatusCode(expectedStatusCode)
-                .expectResponseTime(lessThanOrEqualTo(3000L))
+                .expectResponseTime(lessThanOrEqualTo(RESPONSE_TIME))
                 .build();
     }
 }
